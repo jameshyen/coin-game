e6de7d5a3bcc4681bdabbc288b7d517a6622c550
James Yen <jameshyen@gmail.com>
2017-02-09 22:06:11 -0800

    bluebird

--- server/game.js
+++ server/game.js
@@ -12,8 +12,8 @@ const { clamp, randomPoint, permutation } = require('./gameutil');
 // https://www.npmjs.com/package/redis
 const redis = require('redis');
 
-// const bluebird = require('bluebird');
-// bluebird.promisifyAll(redis.RedisClient.prototype);
+const bluebird = require('bluebird');
+bluebird.promisifyAll(redis.RedisClient.prototype);
 
 const client = redis.createClient();
 client.on('error', err => console.log(`Error ${err}`));
@@ -45,14 +45,14 @@ const NUM_COINS = 100;
 }; */
 
 exports.addPlayer = (name, io, socket) => {
-  client.sismember('usednames', name, (err, res) => {
+  return client.sismemberAsync('usednames', name).then((res) => {
     if (res === 1 || name.length === 0 || name.length > MAX_PLAYER_NAME_LENGTH) {
-      io.to(socket.id).emit('goodname', false);
+      return false;
     } else {
-      client.sadd('usednames', name, (err2, res2) => {
-        client.set(`player:${name}`, randomPoint(WIDTH, HEIGHT).toString(), (err3, res3) => {
-          client.zadd('scores', 0, name, (err4, res4) => {
-            io.to(socket.id).emit('goodname', true);
+      return client.saddAsync('usednames', name).then((res2) => {
+        return client.setAsync(`player:${name}`, randomPoint(WIDTH, HEIGHT).toString()).then((res3) => {
+          return client.zaddAsync('scores', 0, name).((res4) => {
+            return true;
           });
         });
       });
@@ -64,7 +64,7 @@ function placeCoins() {
   permutation(WIDTH * HEIGHT).slice(0, NUM_COINS).forEach((position, i) => {
     const coinValue = (i < 50) ? 1 : (i < 75) ? 2 : (i < 95) ? 5 : 10;
     const index = `${Math.floor(position / WIDTH)},${Math.floor(position % WIDTH)}`;
-    client.hset('coins', index, coinValue);
+    return client.hsetAsync('coins', index, coinValue).then((res) => {});
   });
 }
 
@@ -72,49 +72,53 @@ function placeCoins() {
 // the positions of each player, the scores, and the positions (and values) of each coin.
 // Note that we return the scores in sorted order, so the client just has to iteratively
 // walk through an array of name-score pairs and render them.
-const state = exports.state = (io) => {
+exports.state = () => {
   const positions = [];
   const coins = [];
   const scores = [];
-  client.keys('player:*', (err, res) => {
-    res.forEach(key => client.get(key, (err2, res2) => {
-      positions.push([key.substring(7), res2]);
-      client.zrevrange('scores', 0, -1, 'WITHSCORES', (err3, res3) => {
-        for (let i = 0; i < res3.length; i += 2) {
-          scores[i] = [res3[i], res3[i + 1]];
-        }
-        client.hkeys('coins', (err4, res4) => {
-          res4.forEach(key2 => client.hget('coins', key2, (err5, res5) => {
-            coins.push([key2, res5]);
-            io.emit('state', {
-              positions,
-              scores,
-              coins,
+  return client.keysAsync('player:*').then((res) => {
+    res.forEach((key) => {
+      return client.getAsync(key).then((res2) => {
+        positions.push([key.substring(7), res2]);
+        return client.zrevrangeAsync('scores', 0, -1, 'WITHSCORES').then((res3) => {
+          for (let i = 0; i < res3.length; i += 2) {
+            scores[i] = [res3[i], res3[i + 1]];
+          }
+          return client.hkeysAsync('coins').then((res4) => {
+            res4.forEach((key2) => {
+              return client.hgetAsync('coins', key2).then((res5) => {
+                coins.push([key2, res5]);
+                return {
+                  positions,
+                  scores,
+                  coins
+                };
+              });
             });
-          }));
+          });
         });
       });
-    }));
-  });
-};
+    });
+  };
+}
 
-exports.move = (direction, name, io) => {
+exports.move = (direction, name) => {
   const delta = { U: [0, -1], R: [1, 0], D: [0, 1], L: [-1, 0] }[direction];
   if (delta) {
     const playerKey = `player:${name}`;
     let [x, y] = [0, 0];
-    client.get(playerKey, (err, res) => {
+    return client.getAsync(playerKey).then((res) => {
       [x, y] = res.split(',');
       const [newX, newY] = [clamp(+x + delta[0], 0, WIDTH - 1), clamp(+y + delta[1], 0, HEIGHT - 1)];
-      client.hget('coins', `${newX},${newY}`, (err2, res2) => {
+      return client.hgetAsync('coins', `${newX},${newY}`).then((res2) => {
         if (res2) {
-          client.zincrby('scores', res2, name, (err3, res3) => {
-            client.hdel('coins', `${newX}, ${newY}`);
+          return client.zincrbyAsync('scores', res2, name).then((res3) => {
+            return client.hdelAsync('coins', `${newX}, ${newY}`);
           });
         }
-        client.set(playerKey, `${newX},${newY}`, (err3, res3) => {
+        return client.setAsync(playerKey, `${newX},${newY}`).then((res3) => {
           // When all coins collected, generate a new batch.
-          client.hlen('coins', (err4, res4) => {
+          return client.hlenAsync('coins').then((res4) => {
             if (res4 === 0) {
               placeCoins();
             }
@@ -123,7 +127,6 @@ exports.move = (direction, name, io) => {
       });
     });
   }
-  state(io);
 };
 
 placeCoins();
